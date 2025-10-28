9e6845cb324ddb09e8df11bf2e8144d0afed0191
msheepy <monelson@ualberta.ca>
2025-10-28 17:27:28 -0600

    Made failing test

--- app/src/main/java/com/example/lab08/CustomList.java
+++ app/src/main/java/com/example/lab08/CustomList.java
@@ -14,7 +14,10 @@ public class CustomList {
         cities.add(city);
     }
 
-    
+//    public boolean hasCity(City calgary) {
+//        return cities.contains(city);
+//    }
+
 
     // Will be implemented later using TDD (leave blank for now)
     // public boolean hasCity(City city) { ... }
